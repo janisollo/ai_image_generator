a008036ff06e6a8535e7c25257c652b5d5848116
janisollo <sollo.jani@gmail.com>
2023-03-07 10:19:35 +0200

    Components created and exported to Home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import { Loader, Card, FormField } from "../components";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">
+          The Community Showcase
+        </h1>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
