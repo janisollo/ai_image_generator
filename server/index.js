ac881c3c67cd9afeaee3be2252cc0650ded09838
janisollo <sollo.jani@gmail.com>
2023-03-13 11:48:26 +0200

    dalleRoutes Json was broken, fixed

--- server/index.js
+++ server/index.js
@@ -3,8 +3,8 @@ import express from "express"; // Express is a web framework for Node.js
 import * as dotenv from "dotenv"; // Dotenv is a zero-dependency module that loads environment variables from a .env file
 import cors from "cors"; // Cors is a Node.js package that enables cross-origin resource sharing (CORS)
 import connectDB from "./mongodb/connect.js";
-import postRoutes from "./routes/postRoutes";
-import dalleRoutes from "./routes/dalleRoutes";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
 // Loading environment variables from .env file
 dotenv.config();
 
@@ -16,6 +16,9 @@ app.use(cors());
 
 // Adding middleware to parse incoming requests with JSON payloads
 app.use(express.json({ limit: "50mb" }));
+
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
 
 // Creating a new route for the root endpoint
 app.get("/", async (req, res) => {
