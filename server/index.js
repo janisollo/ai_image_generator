07c7f7a84deff2dd0d3543964989e4abb1e32fbe
janisollo <sollo.jani@gmail.com>
2023-03-13 08:51:59 +0200

    mongoose added

--- server/index.js
+++ server/index.js
@@ -1,22 +1,38 @@
-import express from "express";
-import * as dotenv from "dotenv";
-import cors from "cors";
+// Importing the necessary packages
+import express from "express"; // Express is a web framework for Node.js
+import * as dotenv from "dotenv"; // Dotenv is a zero-dependency module that loads environment variables from a .env file
+import cors from "cors"; // Cors is a Node.js package that enables cross-origin resource sharing (CORS)
+import connectDB from "./mongodb/connect";
 
+// Loading environment variables from .env file
 dotenv.config();
 
+// Creating a new Express application
 const app = express();
 
+// Adding CORS middleware to the Express app to allow cross-origin requests
 app.use(cors());
+
+// Adding middleware to parse incoming requests with JSON payloads
 app.use(express.json({ limit: "50mb" }));
 
+// Creating a new route for the root endpoint
 app.get("/", async (req, res) => {
+  // Sending a simple message as the response
   res.send("Hello from Dall-E");
 });
 
+// Function to start the Express app and listen on port 8080
 const startServer = async () => {
-  app.listen(8080, () =>
-    console.log("Server has started on port http://localhost:8080")
-  );
+  try {
+    connectDB(process.env.MOBGOBD_URL);
+    app.listen(8080, () =>
+      console.log("Server has started on port http://localhost:8080")
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
+// Calling the startServer function to start the Express app
 startServer();
